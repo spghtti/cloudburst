c4779b182eed339b65379d77c0b7351b32b34836
Matt Daniele <2266075+spghtti@users.noreply.github.com>
2021-12-06 21:50:10 -0500

    Hourly forecasting

--- src/forecast.js
+++ src/forecast.js
@@ -1,15 +1,26 @@
 // app ID e6ea60ae42c4f7ea5dbffec273b1f3a0
 import units from './unit';
-import { getTime } from './detailedWeather';
 
 function fillHours(currentUnixTime) {
+  const headlines = document.getElementsByClassName('hourly-headline');
   let newTime = currentUnixTime;
 
-  for (let i = 1; i <= 12; i++) {
+  for (let i = 0; i < 12; i++) {
     newTime += 60 * 60;
     const date = new Date(newTime * 1000);
     const hours = date.getHours();
-    console.log(hours);
+    headlines[i].innerHTML = `${hours}:00`;
+  }
+}
+
+function setHourlyWeather(response) {
+  const icons = document.getElementsByClassName('hourly-weather-icon');
+  const temperatures = document.getElementsByClassName('hourly-temperature');
+  for (let i = 0; i < 12; i++) {
+    icons[
+      i
+    ].src = `http://openweathermap.org/img/wn/${response.hourly[i].weather[0].icon}@2x.png`;
+    temperatures[i].innerHTML = `${Math.round(response.hourly[i].temp)}°`;
   }
 }
 
@@ -22,6 +33,7 @@ function getForecast(lat, lon) {
     .then((response) => {
       console.log(response);
       fillHours(response.current.dt + response.timezone_offset);
+      setHourlyWeather(response);
     })
     .catch((err) => {
       console.log(err);
