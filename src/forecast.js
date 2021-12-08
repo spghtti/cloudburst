d2a9babf9bfafe5ee42bfec8ebd76b8310cd341f
Matt Daniele <2266075+spghtti@users.noreply.github.com>
2021-12-07 21:15:29 -0500

    Added daily forecast

--- src/forecast.js
+++ src/forecast.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-plusplus */
 // app ID e6ea60ae42c4f7ea5dbffec273b1f3a0
 import units from './unit';
 
@@ -24,6 +27,41 @@ function setHourlyWeather(response) {
   }
 }
 
+const weekday = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+function fillWeek(currentUnixTime) {
+  const days = document.getElementsByClassName('day');
+  let newTime = currentUnixTime;
+
+  for (let i = 0; i < 7; i++) {
+    newTime += 24 * 60 * 60;
+    const date = new Date(newTime * 1000);
+    const day = weekday[date.getDay()];
+    days[i].innerHTML = day;
+  }
+}
+
+function setWeekWeather(response) {
+  const icons = document.getElementsByClassName('week-weather-icon');
+  const temperatures = document.getElementsByClassName('day-temperature');
+
+  for (let i = 1; i < 8; i++) {
+    icons[
+      i - 1
+    ].src = `http://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}@2x.png`;
+    temperatures[i - 1].innerHTML = `${Math.round(
+      response.daily[i].temp.max
+    )}°`;
+  }
+}
+
 function getForecast(lat, lon) {
   fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=${units.unit}&appid=e6ea60ae42c4f7ea5dbffec273b1f3a0`,
@@ -34,6 +72,8 @@ function getForecast(lat, lon) {
       console.log(response);
       fillHours(response.current.dt + response.timezone_offset);
       setHourlyWeather(response);
+      fillWeek(response.current.dt + response.timezone_offset);
+      setWeekWeather(response);
     })
     .catch((err) => {
       console.log(err);
